993dc0795c8b97d5fb2b22af3403088fa6b3697c
Spencer17x <1253478653@qq.com>
2022-07-08 16:48:19 +0800

    feat(live-demo): 列表页

--- packages/live-demo/src/pages/room/index.tsx
+++ packages/live-demo/src/pages/room/index.tsx
@@ -2,8 +2,6 @@ import { UA } from '@/utils';
 import { RoomPC } from './pc';
 import { RoomMobile } from './mobile';
 
-
-
 export const Room = () => {
   return UA.isPC ? <RoomPC/> : <RoomMobile/>;
 };
