492c31e202259b94362ed59c7d0acf5bd2dba3e1
Spencer17x <1253478653@qq.com>
2022-07-11 18:41:31 +0800

    feat(live-demo): 登录页添加版本号

--- packages/live-demo/vite.config.ts
+++ packages/live-demo/vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
+const packageJson = require('./package.json');
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
+    define: {
+      version: JSON.stringify(packageJson.version),
+    },
     plugins: [react()],
     resolve: {
       alias: {
