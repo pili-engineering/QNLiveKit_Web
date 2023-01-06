c4bbdbc51bf9fca8d7a9be737b8e0a5bb9747a71
Spencer17x <1253478653@qq.com>
2023-01-06 11:31:40 +0800

    chore(*): 修改scopes顺序

--- changelog.config.js
+++ changelog.config.js
@@ -1,68 +1,86 @@
 const fs = require('fs');
 
-const projects = fs.readdirSync('./packages').filter(projectName => {
-  return fs.statSync(`./packages/${projectName}`).isDirectory();
+const projects = fs.readdirSync('./packages').filter((projectName) => {
+	return fs.statSync(`./packages/${projectName}`).isDirectory();
 });
 
-
 module.exports = {
-  disableEmoji: true,
-  format: '{type}{scope}: {emoji}{subject}',
-  list: ['test', 'feat', 'fix', 'chore', 'docs', 'refactor', 'style', 'ci', 'perf', 'release'],
-  maxMessageLength: 64,
-  minMessageLength: 3,
-  questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
-  scopes: projects.concat(''),
-  types: {
-    chore: {
-      description: 'Build process or auxiliary tool changes',
-      emoji: '🤖',
-      value: 'chore',
-    },
-    ci: {
-      description: 'CI related changes',
-      emoji: '🎡',
-      value: 'ci',
-    },
-    docs: {
-      description: 'Documentation only changes',
-      emoji: '✏️',
-      value: 'docs',
-    },
-    feat: {
-      description: 'A new feature',
-      emoji: '🎸',
-      value: 'feat',
-    },
-    fix: {
-      description: 'A bug fix',
-      emoji: '🐛',
-      value: 'fix',
-    },
-    perf: {
-      description: 'A code change that improves performance',
-      emoji: '⚡️',
-      value: 'perf',
-    },
-    refactor: {
-      description: 'A code change that neither fixes a bug or adds a feature',
-      emoji: '💡',
-      value: 'refactor',
-    },
-    release: {
-      description: 'Create a release commit',
-      emoji: '🏹',
-      value: 'release',
-    },
-    style: {
-      description: 'Markup, white-space, formatting, missing semi-colons...',
-      emoji: '💄',
-      value: 'style',
-    },
-    test: {
-      description: 'Adding missing tests',
-      emoji: '💍',
-      value: 'test',
-    },
-  },
+	disableEmoji: true,
+	format: '{type}{scope}: {emoji}{subject}',
+	list: [
+		'test',
+		'feat',
+		'fix',
+		'chore',
+		'docs',
+		'refactor',
+		'style',
+		'ci',
+		'perf',
+		'release'
+	],
+	maxMessageLength: 64,
+	minMessageLength: 3,
+	questions: [
+		'type',
+		'scope',
+		'subject',
+		'body',
+		'breaking',
+		'issues',
+		'lerna'
+	],
+	scopes: ['*'].concat(projects),
+	types: {
+		chore: {
+			description: 'Build process or auxiliary tool changes',
+			emoji: '🤖',
+			value: 'chore'
+		},
+		ci: {
+			description: 'CI related changes',
+			emoji: '🎡',
+			value: 'ci'
+		},
+		docs: {
+			description: 'Documentation only changes',
+			emoji: '✏️',
+			value: 'docs'
+		},
+		feat: {
+			description: 'A new feature',
+			emoji: '🎸',
+			value: 'feat'
+		},
+		fix: {
+			description: 'A bug fix',
+			emoji: '🐛',
+			value: 'fix'
+		},
+		perf: {
+			description: 'A code change that improves performance',
+			emoji: '⚡️',
+			value: 'perf'
+		},
+		refactor: {
+			description: 'A code change that neither fixes a bug or adds a feature',
+			emoji: '💡',
+			value: 'refactor'
+		},
+		release: {
+			description: 'Create a release commit',
+			emoji: '🏹',
+			value: 'release'
+		},
+		style: {
+			description: 'Markup, white-space, formatting, missing semi-colons...',
+			emoji: '💄',
+			value: 'style'
+		},
+		test: {
+			description: 'Adding missing tests',
+			emoji: '💍',
+			value: 'test'
+		}
+	}
 };
